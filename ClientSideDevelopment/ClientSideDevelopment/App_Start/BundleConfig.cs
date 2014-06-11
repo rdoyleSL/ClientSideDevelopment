5affd553973f09a0364fa4c5e9047a51abe6641f
DevDigital <devdigital.cloud@gmail.com>
2014-06-11 10:10:19 +0100

    Added Abide validation

--- ClientSideDevelopment/ClientSideDevelopment/App_Start/BundleConfig.cs
+++ ClientSideDevelopment/ClientSideDevelopment/App_Start/BundleConfig.cs
@@ -18,6 +18,10 @@ namespace ClientSideDevelopment
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
+                        "~/Scripts/foundation/foundation.js",
+                        "~/Scripts/foundation/foundation.abide.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
