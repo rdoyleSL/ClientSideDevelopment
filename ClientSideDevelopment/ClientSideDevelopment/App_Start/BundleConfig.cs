bdde2e16f1ca84f5c7fe8d9abe36b6e47385b58f
DevDigital <devdigital.cloud@gmail.com>
2014-06-11 11:02:05 +0100

    Added Foundation icons for related titles

--- ClientSideDevelopment/ClientSideDevelopment/App_Start/BundleConfig.cs
+++ ClientSideDevelopment/ClientSideDevelopment/App_Start/BundleConfig.cs
@@ -27,7 +27,9 @@ namespace ClientSideDevelopment
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/site.css",
+                        "~/Content/foundation-icons.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
