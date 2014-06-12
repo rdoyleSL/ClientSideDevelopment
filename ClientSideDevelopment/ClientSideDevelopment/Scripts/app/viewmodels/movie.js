04ba96003284dbd96fd2a4f000fadc1acb6cb819
DevDigital <devdigital.cloud@gmail.com>
2014-06-12 13:38:26 +0100

    Related movies now loaded

--- ClientSideDevelopment/ClientSideDevelopment/Scripts/app/viewmodels/movie.js
+++ ClientSideDevelopment/ClientSideDevelopment/Scripts/app/viewmodels/movie.js
@@ -2,14 +2,49 @@
     "use strict";
 
     filmSite.MovieViewModel = (function (id, title, criticsConsensus) {
-        id = ko.observable(id);
-        title = ko.observable(title);
-        criticsConsensus = ko.observable(criticsConsensus);
-        
+        id = ko.observable(id || null);
+        title = ko.observable(title || null);
+        criticsConsensus = ko.observable(criticsConsensus || null);
+
+        var relatedTitles = ko.observableArray([]);
+
+        function loadRelatedTitles() {
+            if (id() === null) {
+                return;
+            }
+
+            $.ajax({
+                url: "http://api.rottentomatoes.com/api/public/v1.0/movies/" + id() + "/similar.json?apikey=jvasufnuxk3mhhghbmcq9a2h&callback=?",
+                type: "GET",
+                dataType: "jsonp",
+                success: function (related) {
+                    relatedTitles.removeAll();
+                    for (var i = 0; i < related.movies.length; i++) {
+                        var movie = related.movies[i];
+                        relatedTitles.push(new filmSite.RelatedMovieInfoViewModel(
+                                movie.posters.thumbnail,
+                                movie.title,
+                                movie.year,
+                                movie.ratings.audience_score,
+                                movie.ratings.critics_score
+                            ));
+
+                        //$("#related-titles-table").append("<tr><td><img class=\"th radius\" src=\"" + movie.posters.thumbnail + "\"></img></td><td>" + movie.title + "</td><td>" + movie.year + "</td><td>" + movie.ratings.audience_score + "%</td><td>" + movie.ratings.critics_score + "%</td></tr>");
+                    }
+                },
+                error: function (xhr, status, error) {
+                    alert(status);
+                }
+            });
+        }
+
+        loadRelatedTitles();
+
         return {
             id: id,
             title: title,
-            criticsConsensus: criticsConsensus
+            criticsConsensus: criticsConsensus,
+            relatedTitles: relatedTitles
         }
     });
 
---
+++ ClientSideDevelopment/ClientSideDevelopment/Scripts/app/viewmodels/related-movie.js
@@ -0,0 +1,20 @@
+﻿(function (filmSite) {
+    "use strict";
+
+    filmSite.RelatedMovieInfoViewModel = (function (poster, title, year, audienceScore, criticsScore) {
+        poster = ko.observable(poster);
+        title = ko.observable(title);
+        year = ko.observable(year);
+        audienceScore = ko.observable(audienceScore);
+        criticsScore = ko.observable(criticsScore);
+
+        return {
+            poster: poster,
+            title: title,
+            year: year,
+            audienceScore: audienceScore,
+            criticsScore: criticsScore
+        }
+    });
+
+})(window.filmSite = window.filmSite || {});
